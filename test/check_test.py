8ce503941af3bca185d1acc068758e499395914b
llirik1552 <llirik1552@gmail.com>
2025-06-19 21:01:28 +0300

    kirill_1

--- test/check_test.py
+++ test/check_test.py
@@ -2,10 +2,11 @@ import requests
 
 # Создать задачу. Проставить отметку о выполнении и проверить что completed ==True
 def test_add():
-    body = {"title": "generated", "completed": True}
+    body = {"title": "generated", "completed": False}
     response = requests.post("https://sky-todo-list.herokuapp.com/", json=body)
-    response_body = response.json()
+    body1 = {"title": "generated", "completed": True}
+    response_check = requests.patch("https://sky-todo-list.herokuapp.com/", json=body1)
 
     assert response.status_code == 200
-    assert response_body['completed'] == True
+    assert response_check['completed'] == True
 
