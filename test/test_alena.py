e20a3a24f59e0a1c4d517e1f170da3b98106a99c
girlofmelody91 <girlofmelody91@gmail.com>
2025-06-19 20:33:18 +0300

    изменено

--- test/test_alena.py
+++ test/test_alena.py
@@ -14,8 +14,3 @@ def test_edit():
     assert response.status_code == 200
     assert response.json()['title'] == "generated-1"
 
-
-def get_tasks_test():
-    response = requests.get("https://todo-app-sky.herokuapp.com/")
-    assert response.status_code == 200
-    assert type(response.json()) is list
